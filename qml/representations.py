e32e6562b7fd745e696b9cf282b5ce89527398ee
Lars Andersen Bratholm <larsbratholm@gmail.com>
2017-06-14 13:54:38 +0200

    python3 fix

--- qml/representations.py
+++ qml/representations.py
@@ -34,21 +34,6 @@ from .frepresentations import fgenerate_bob
 from .data import NUCLEAR_CHARGE
 
 def generate_coulomb_matrix(nuclear_charges, coordinates, size = 23, sorting = "row-norm"):
-    """ Generates a sorted molecular coulomb matrix, sorted either by ``"row-norm"`` or ``"unsorted"``.
-    ``size=`` denotes the max number of atoms in the molecule (thus the size of the resulting square matrix.
-    The resulting matrix is the upper triangle put into the form of a 1D-vector.
-
-    :param nuclear_charges: List of nuclear charges.
-    :type nuclear_charges: numpy array
-    :param coordinates: Input coordinates.
-    :type coordinates: numpy array
-    :param size: Max number of atoms in representation.
-    :type size: integer
-    :param sorting: Matrix sorting scheme, "row-norm" or "unsorted".
-    :type sorting: string
-    :return: 1D Coulomb matrix representation
-    :rtype: numpy array
-    """
 
     if (sorting == "row-norm"):
         return fgenerate_coulomb_matrix(nuclear_charges, \
@@ -63,21 +48,6 @@ def generate_coulomb_matrix(nuclear_charges, coordinates, size = 23, sorting = "
         raise SystemExit
 
 def generate_atomic_coulomb_matrix(nuclear_charges, coordinates, size = 23, sorting = "distance"):
-    """ Generates a list of sorted Coulomb matrices, sorted either by ``"row-norm"`` or ``"distance"``, the latter refers to sorting by distance to each query atom.
-    ``size=`` denotes the max number of atoms in the molecule (thus the size of the resulting square matrix.
-    The resulting matrix is the upper triangle put into the form of a 1D-vector.
-
-    :param nuclear_charges: List of nuclear charges.
-    :type nuclear_charges: numpy array
-    :param coordinates: Input coordinates.
-    :type coordinates: numpy array
-    :param size: Max number of atoms in representation.
-    :type size: integer
-    :param sorting: Matrix sorting scheme, "row-norm" or "distance".
-    :type sorting: string
-    :return: List of 1D Coulomb matrix representations.
-    :rtype: numpy array
-    """
 
     if (sorting == "row-norm"):
         return fgenerate_local_coulomb_matrix(nuclear_charges,
@@ -92,39 +62,10 @@ def generate_atomic_coulomb_matrix(nuclear_charges, coordinates, size = 23, sort
         raise SystemExit
 
 def generate_eigenvalue_coulomb_matrix(nuclear_charges, coordinates, size = 23):
-    """ Generates an eigenvalue molecular coulomb matrix, sorted by descending values.
-    ``size=`` denotes the max number of atoms in the molecule (thus the size of the resulting square matrix.
-    The resulting matrix is in the form of a 1D-vector.
-
-    :param nuclear_charges: List of nuclear charges.
-    :type nuclear_charges: numpy array
-    :param coordinates: Input coordinates.
-    :type coordinates: numpy array
-    :param size: Max number of atoms in representation.
-    :type size: integer
-    :return: 1D Coulomb matrix representation
-    :rtype: numpy array
-    """
     return fgenerate_eigenvalue_coulomb_matrix(nuclear_charges,
         coordinates, size)
 
 def generate_bob(nuclear_charges, coordinates, atomtypes, asize = {"O":3, "C":7, "N":3, "H":16, "S":1}):
-    """ Generates a bag-of-bonds (BOB) representation of the molecule.
-    ``asize=`` is the maximum number of atoms of each type (necessary to generate bags of minimal sizes).
-    The resulting matrix is the BOB representation put into the form of a 1D-vector.
-    Reference: 10.1021/acs.jpclett.5b00831
-
-    :param nuclear_charges: List of nuclear charges.
-    :type nuclear_charges: numpy array
-    :param coordinates: Input coordinates.
-    :type coordinates: numpy array
-    :param size: Max number of atoms in representation.
-    :type size: integer
-    :param asize: Max number of each element type.
-    :type asize: dict
-    :return: 1D BOB representation.
-    :rtype: numpy array
-    """
 
     n = 0
     ids = np.zeros(len(asize), dtype=int)
@@ -132,7 +73,7 @@ def generate_bob(nuclear_charges, coordinates, atomtypes, asize = {"O":3, "C":7,
         n += value * (1+value)
         ids[i] = NUCLEAR_CHARGE[key]
         for j in range(i):
-            v = asize.values()[j]
+            v = list(asize.values())[j]
             n += 2 * value * v
     n /= 2
 
