b36d0b567e89256785d28e61c38d0dda33e1d077
Anders S. Christensen <andersbiceps@gmail.com>
2017-07-07 12:02:39 +0200

    Added busch-kaufman factorization solver and inversion (DSYTRS)

--- qml/math.py
+++ qml/math.py
@@ -26,6 +26,8 @@ from copy import deepcopy
 
 from .fcho_solve import fcho_solve
 from .fcho_solve import fcho_invert
+from .fcho_solve import fbkf_solve
+from .fcho_solve import fbkf_invert
 
 
 def cho_invert(A):
@@ -79,11 +81,80 @@ def cho_solve(A, y):
 
     n = A.shape[0]
 
+    # Backup diagonal before Cholesky-decomposition
     A_diag = A[np.diag_indices_from(A)]
 
     x = np.zeros(n)
     fcho_solve(A, y, x)
 
+    # Reset diagonal after Cholesky-decomposition
+    A[np.diag_indices_from(A)] = A_diag
+
+    # Copy lower triangle to upper
+    i_lower = np.tril_indices_from(A)
+    A.T[i_lower] = A[i_lower]
+
+    return x
+
+
+def bkf_invert(A):
+    """ Returns the inverse of a positive definite matrix, using a Cholesky decomposition
+        via calls to LAPACK dpotrf and dpotri in the F2PY module.
+
+        :param A: Matrix (symmetric and positive definite, left-hand side).
+        :type A: numpy array
+
+        :return: The inverse matrix
+        :rtype: numpy array
+    """
+
+    if len(A.shape) != 2 or A.shape[0] != A.shape[1]:
+        raise ValueError('expected square matrix')
+
+    I = np.asfortranarray(A)
+
+    fbkf_invert(I)
+
+    # Matrix to store the inverse
+    i_lower = np.tril_indices_from(A)
+
+    # Copy lower triangle to upper
+    I.T[i_lower] = I[i_lower]
+
+    return I
+
+
+def bkf_solve(A, y):
+    """ Solves the equation
+
+            :math:`A x = y`
+
+        for x using a Cholesky decomposition  via calls to LAPACK dpotrf and dpotrs in the F2PY module. Preserves the input matrix A.
+
+        :param A: Matrix (symmetric and positive definite, left-hand side).
+        :type A: numpy array
+        :param y: Vector (right-hand side of the equation).
+        :type y: numpy array
+
+        :return: The solution vector.
+        :rtype: numpy array
+        """
+
+    if len(A.shape) != 2 or A.shape[0] != A.shape[1]:
+        raise ValueError('expected square matrix')
+
+    if len(y.shape) != 1 or y.shape[0] != A.shape[1]:
+        raise ValueError('expected matrix and vector of same stride size')
+
+    n = A.shape[0]
+
+    # Backup diagonal before Cholesky-decomposition
+    A_diag = A[np.diag_indices_from(A)]
+
+    x = np.zeros(n)
+    fbkf_solve(A, y, x)
+
+    # Reset diagonal after Cholesky-decomposition
     A[np.diag_indices_from(A)] = A_diag
 
     # Copy lower triangle to upper
--- tests/test_math.py
+++ tests/test_math.py
@@ -69,6 +69,51 @@ def test_cho_invert():
     # Check that it is a true inverse
     assert np.allclose(np.matmul(A, Ai_qml), one, atol=1e-7)
 
+
+def test_bkf_invert():
+
+    test_dir = os.path.dirname(os.path.realpath(__file__))
+
+    A_ref = np.loadtxt(test_dir + "/data/K_local_gaussian.txt")
+
+    A = deepcopy(A_ref) 
+    Ai_qml = qml.math.bkf_invert(A)
+
+    # Check A is unchanged
+    assert np.allclose(A, A_ref)
+
+    A = deepcopy(A_ref) 
+    one = np.eye(A.shape[0])
+   
+    np.set_printoptions(linewidth=20000)
+    assert np.allclose(np.matmul(A, Ai_qml), one, atol=1e-7)
+
+
+def test_bkf_solve():
+
+    test_dir = os.path.dirname(os.path.realpath(__file__))
+
+    A_ref = np.loadtxt(test_dir + "/data/K_local_gaussian.txt")
+    y_ref = np.loadtxt(test_dir + "/data/y_cho_solve.txt")
+
+    A = deepcopy(A_ref) 
+    y = deepcopy(y_ref)
+    x_qml   = qml.math.bkf_solve(A,y)
+
+    # Check arrays are unchanged
+    assert np.allclose(y, y_ref)
+    assert np.allclose(A, A_ref)
+
+    A = deepcopy(A_ref) 
+    y = deepcopy(y_ref) 
+    x_scipy = np.linalg.solve(A, y)
+
+    # Check for correct solution
+    assert np.allclose(x_qml, x_scipy)
+
+
 if __name__ == "__main__":
     test_cho_solve()
     test_cho_invert()
+    test_bkf_invert()
+    test_bkf_solve()
