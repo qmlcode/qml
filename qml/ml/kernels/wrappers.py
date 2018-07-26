70801a5f0f58f455de96095c4a5d760be4723c20
larsbratholm <larsbratholm@gmail.com>
2018-07-26 16:45:22 +0100

    Added licence to all files

--- qml/ml/kernels/wrappers.py
+++ qml/ml/kernels/wrappers.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2016 Anders Steen Christensen, Felix A. Faber, Lars A. Bratholm
+# Copyright (c) 2016-2017 Anders Steen Christensen, Felix A. Faber, Lars A. Bratholm
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
--- qml/ml/representations/representations.py
+++ qml/ml/representations/representations.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2017 Anders Steen Christensen, Lars Andersen Bratholm and Bing Huang
+# Copyright (c) 2017-2018 Anders Steen Christensen, Lars Andersen Bratholm, Bing Huang
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
