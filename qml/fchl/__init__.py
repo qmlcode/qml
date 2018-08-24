6dff232f592a68a7cf7c530ce06a3a45c7c6b648
Anders S. Christensen <andersbiceps@gmail.com>
2018-08-24 15:59:32 +0200

    Updated manual for FCHL functionality - still needs tutorial/examples pages

--- qml/fchl/__init__.py
+++ qml/fchl/__init__.py
@@ -20,8 +20,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from __future__ import absolute_import
-
 from .fchl_representations import *
 from .fchl_scalar_kernels import *
 from .fchl_force_kernels import *
