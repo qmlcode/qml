4b785f18422d38cc8dd416c9e779c72092040b8c
larsbratholm <larsbratholm@gmail.com>
2018-09-28 15:07:43 +0100

    Moved get_batch_size back into utils, since it doesn't depend on tf

--- qml/aglaia/tf_utils.py
+++ qml/aglaia/tf_utils.py
@@ -120,16 +120,3 @@ def generate_weights(n_in, n_out, hl):
 
     return weights, biases
 
-def get_batch_size(batch_size, n_samples):
-
-    if batch_size > n_samples:
-        print("Warning: batch_size larger than sample size. It is going to be clipped")
-        return min(n_samples, batch_size)
-
-    # see if the batch size can be modified slightly to make sure the last batch is similar in size
-    # to the rest of the batches
-    # This is always less that the requested batch size, so no memory issues should arise
-
-    better_batch_size = ceil(n_samples, ceil(n_samples, batch_size))
-    return better_batch_size
-
