68ef9ea3d7292be5713004114df92ef5f9ef12a4
kvkarandashev <kvkarandashev@gmail.com>
2021-03-04 19:32:30 +0100

    Updated files' headers.

--- qml/representations/facsf.f90
+++ qml/representations/facsf.f90
@@ -1,6 +1,6 @@
 ! MIT License
 !
-! Copyright (c) 2018 Lars Andersen Bratholm
+! Copyright (c) 2018-2021 Lars Andersen Bratholm, Konstantin Karandashev
 !
 ! Permission is hereby granted, free of charge, to any person obtaining a copy
 ! of this software and associated documentation files (the "Software"), to deal
--- qml/representations/representations.py
+++ qml/representations/representations.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2017-2018 Anders Steen Christensen, Lars Andersen Bratholm, Bing Huang
+# Copyright (c) 2017-2021 Anders Steen Christensen, Lars Andersen Bratholm, Bing Huang, Konstantin Karandashev
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
